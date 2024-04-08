1a50384269c135efb20d32f7c4451e42aa779600
hassanwaqa <hassanwaqa277@gmail.com>
2024-04-08 17:05:32 +0500

    Redux

--- src/App.js
+++ src/App.js
@@ -1,10 +1,36 @@
-import './App.css';
+// Run On Node
 
-function App() {
-  return (
-    <>
-    </>
-  );
+const redux = require('redux')
+const createStore = redux.createStore;
+
+
+const BUY_CAKE = "BUY_CAKE"
+
+const buyCake = () => {
+    return {
+        type : BUY_CAKE,
+        info : "First Redux Action"
+    }
 }
 
-export default App;
+const initialValues = {
+    numOfCakes : 10
+}
+const reducer = (state = initialValues, action) => {
+    switch (action.type) {
+        case BUY_CAKE: return {
+            ...state,
+            numOfCakes: state.numOfCakes -1
+        }
+
+        default : return state
+    }
+}
+
+const store = createStore(reducer)
+console.log("Initial State : ", store.getState())
+const unsubscribe = store.subscribe(()=> console.log("Updated Value : ", store.getState()))
+store.dispatch(buyCake())
+store.dispatch(buyCake())
+store.dispatch(buyCake())
+unsubscribe()
