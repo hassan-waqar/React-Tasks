90f23643440846b9dad9a9af1a94b9c51d015172
hassanwaqa <hassanwaqa277@gmail.com>
2024-04-09 13:45:16 +0500

    Context-Implementation

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
+import About from "./About";
+import NoteState from "./context/NoteState";
 
 function App() {
   return (
-    <>
-    </>
+    <NoteState>
+      <About/>
+    </NoteState>
   );
 }
 
