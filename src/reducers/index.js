788e4004058085f6c8e93571dfe29ddee8d6b701
hassanwaqa <hassanwaqa277@gmail.com>
2024-04-17 19:07:34 +0500

    Redux-Saga

---
+++ src/reducers/index.js
@@ -0,0 +1,13 @@
+
+const reducer = (state = {}, action) => {
+
+    switch (action.type) {
+        case 'GET_NEWS':
+            return { ...state, loading: true };
+        case 'NEWS_RECEIVED':
+            return { ...state, title: action.json.title, body : action.json.body, loading: false }
+        default:
+            return state;
+    }
+};
+export default reducer;
